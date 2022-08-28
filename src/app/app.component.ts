e68c03c780db204f659e400a6a6d81eb7c1a45a7
Abha <abha2584@yahoo.com>
2022-08-28 10:23:36 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProfileEditor';
+  title = 'Profile Editor';
+  subtitle:string = 'User Information';
+  formUser:User[]= [{name:'Abha',age:30,favoriteColor:'pink'}];
+
+ displayEdit:boolean=false;
+
+ toggleEdit():void{
+  this.displayEdit= !this.displayEdit;
+ }
+
+ saveChanges(data: NgForm)
+ {
+  this.formUser[0].name= data.controls["name"].value;
+   this.formUser[0].favoriteColor=data.controls["favoritecolor"].value;
+   this.formUser[0].age=data.controls["age"].value;
+
+ }
 }
